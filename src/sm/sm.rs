de2b9c7f916dc6f2c48786dbb79d525c65655000
sunxiaoning <xiaoning.sun@huolala.cn>
2022-09-20 00:43:18 +0800

    trans action

--- src/sm/sm.rs
+++ src/sm/sm.rs
@@ -1,18 +1,24 @@
 use std::fmt::Debug;
 
-struct TransitionBuilder<S: PartialEq + Clone, E: PartialEq> {
+struct TransitionBuilder<S: PartialEq + Debug + Clone, E: PartialEq> {
     source: Option<State<S>>,
     target: Option<State<S>>,
     event: Option<Event<E>>,
+    action: Option<Action<S, E>>,
 }
 
-impl<S: PartialEq + Clone, E: PartialEq> TransitionBuilder<S, E> {
+impl<S: PartialEq + Debug + Clone, E: PartialEq> TransitionBuilder<S, E> {
     fn build(&mut self) -> Transition<S, E> {
         Transition::new(
             self.source.take().expect("source state absent!"),
             self.target.take().expect("target state absent!"),
             self.event.take().expect("event absent"),
+            self.action.take().expect("action absent"),
         )
+    }
+    fn action(&mut self, act: Action<S, E>) -> &mut Self {
+        self.action = Some(act);
+        self
     }
     fn event(&mut self, event: Event<E>) -> &mut Self {
         self.event = Some(event);
@@ -31,27 +37,41 @@ impl<S: PartialEq + Clone, E: PartialEq> TransitionBuilder<S, E> {
             source: None,
             target: None,
             event: None,
+            action: None,
         }
     }
 }
 
-struct Transition<S: PartialEq + Clone, E: PartialEq> {
+struct StateContext<'a, S: PartialEq + Debug + Clone, E: PartialEq> {
+    tran: &'a Transition<S, E>,
+}
+
+impl<'a, S: PartialEq + Debug + Clone, E: PartialEq> StateContext<'a, S, E> {
+    fn new(tran: &Transition<S, E>) -> StateContext<S, E> {
+        StateContext { tran }
+    }
+}
+
+type Action<S, E> = fn(ctx: &StateContext<S, E>) -> Result<bool, &'static str>;
+
+struct Transition<S: PartialEq + Debug + Clone, E: PartialEq> {
     source: State<S>,
     target: State<S>,
     event: Event<E>,
-    // action: fn() ,
+    action: Action<S, E>,
 }
 
-impl<S: PartialEq + Clone, E: PartialEq> Transition<S, E> {
-    fn transit(&self) -> Result<bool, &str> {
+impl<S: PartialEq + Debug + Clone, E: PartialEq> Transition<S, E> {
+    fn transit(&self, _ctx: &StateContext<S, E>) -> Result<bool, &str> {
         Ok(true)
     }
 
-    fn new(source: State<S>, target: State<S>, event: Event<E>) -> Self {
+    fn new(source: State<S>, target: State<S>, event: Event<E>, action: Action<S, E>) -> Self {
         Transition {
             source,
             target,
             event,
+            action,
         }
     }
 }
@@ -121,7 +141,11 @@ trait Source<S: PartialEq + Debug + Clone, E: PartialEq + Debug> {
 }
 
 trait Target<S: PartialEq + Debug + Debug + Clone, E: PartialEq + Debug> {
-    fn event(self: Box<Self>, event: Event<E>) -> Box<StateMachineBuilder<S, E>>;
+    fn event(self: Box<Self>, event: Event<E>) -> Box<dyn Act<S, E>>;
+}
+
+trait Act<S: PartialEq + Debug + Debug + Clone, E: PartialEq + Debug> {
+    fn action(self: Box<Self>, act: Action<S, E>) -> Box<StateMachineBuilder<S, E>>;
 }
 
 trait TransAdder<S: PartialEq + Debug + Clone, E: PartialEq + Debug> {
@@ -197,14 +221,25 @@ impl<S: PartialEq + Debug + Clone + 'static, E: PartialEq + Debug + 'static> Sou
     }
 }
 
-impl<S: PartialEq + Debug + Clone, E: PartialEq + Debug> Target<S, E>
+impl<S: PartialEq + Debug + Clone + 'static, E: PartialEq + Debug + 'static> Target<S, E>
     for StateMachineBuilder<S, E>
 {
-    fn event(mut self: Box<Self>, event: Event<E>) -> Box<StateMachineBuilder<S, E>> {
+    fn event(mut self: Box<Self>, event: Event<E>) -> Box<dyn Act<S, E>> {
         let tranb = self.trans_builder.as_mut().expect("trans builder absent");
         tranb.source.as_ref().expect("source absent");
-        tranb.source.as_ref().expect("target absent");
+        tranb.target.as_ref().expect("target absent");
         tranb.event(event);
+        self
+    }
+}
+
+impl<S: PartialEq + Debug + Clone, E: PartialEq + Debug> Act<S, E> for StateMachineBuilder<S, E> {
+    fn action(mut self: Box<Self>, act: Action<S, E>) -> Box<StateMachineBuilder<S, E>> {
+        let tranb = self.trans_builder.as_mut().expect("trans builder absent");
+        tranb.source.as_ref().expect("source absent");
+        tranb.target.as_ref().expect("target absent");
+        tranb.event.as_ref().expect("event absent");
+        tranb.action(act);
         self
     }
 }
@@ -270,22 +305,25 @@ impl<'a, S: PartialEq + Debug + Clone, E: PartialEq + Debug> StateMachine<S, E> 
         }
 
         let mut rs = false;
-        for trans in self.trans.iter() {
-            if trans.source.eq(&self.current) && trans.event.eq(e) {
+        for tran in self.trans.iter() {
+            if tran.source.eq(&self.current) && tran.event.eq(e) {
                 let mut err_msg = None;
-                rs = trans.transit().unwrap_or_else(|err| {
+                let state_ctx = StateContext::new(tran);
+                rs = tran.transit(&state_ctx).unwrap_or_else(|err| {
                     eprintln!(
                         "state machine trans: source: {:?}, event: {:?}, err: {}",
-                        trans.source.id, e.id, err
+                        tran.source.id, e.id, err
                     );
                     err_msg = Some(String::from(err.clone()));
                     false
                 });
                 self.err = err_msg;
                 if !rs {
-                    println!("statemachine event: {:?} not accept!", e.id)
+                    println!("statemachine event: {:?} not accept!", e.id);
+                    break;
                 }
-                self.current = trans.target.clone();
+                (tran.action)(&state_ctx).unwrap();
+                self.current = tran.target.clone();
                 break;
             }
         }
@@ -322,6 +360,30 @@ mod tests {
         Timeout,
     }
 
+    fn submit(ctx: &StateContext<OrderState, OrderEvent>) -> Result<bool, &'static str> {
+        println!(
+            "------s: {:?}, t: {:?} submit!-----------",
+            ctx.tran.source.id, ctx.tran.target.id
+        );
+        Ok(true)
+    }
+
+    fn pay(ctx: &StateContext<OrderState, OrderEvent>) -> Result<bool, &'static str> {
+        println!(
+            "s: {:?}, t: {:?} pay!",
+            ctx.tran.source.id, ctx.tran.target.id
+        );
+        Ok(true)
+    }
+
+    fn err(ctx: &StateContext<OrderState, OrderEvent>) -> Result<bool, &'static str> {
+        println!(
+            "s: {:?}, t: {:?} timeout!",
+            ctx.tran.source.id, ctx.tran.target.id
+        );
+        Ok(true)
+    }
+
     fn init_sm<S, E>() -> StateMachine<OrderState, OrderEvent> {
         Box::new(StateMachineBuilder::new())
             .init(State::build(OrderState::I))
@@ -329,14 +391,17 @@ mod tests {
             .source(State::build(OrderState::I))
             .target(State::build(OrderState::P))
             .event(Event::build(OrderEvent::Submit))
+            .action(submit)
             .and()
             .source(State::build(OrderState::P))
             .target(State::build(OrderState::S))
             .event(Event::build(OrderEvent::Payment))
+            .action(pay)
             .and()
             .source(State::build(OrderState::P))
             .target(State::build(OrderState::F))
             .event(Event::build(OrderEvent::Timeout))
+            .action(err)
             .done()
             .end(State::build(OrderState::S))
             .build()
